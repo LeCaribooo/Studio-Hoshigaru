15618a00ba04f96a139eaacb1ae4b520f07c198f
Benoit <benoit.burg@epita.fr>
2021-01-27 00:04:37 +0100

    Début du test pour les skills trees

---
+++ Studio Hoshigaru/Assets/Test Benoit/Skill Tree/Talent Tree/Talent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talent : MonoBehaviour
+{
+    private Image sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    public void Lock()
+    {
+        sprite.color = Color.gray;
+    }
+
+    public void Unlock()
+    {
+        sprite.color = Color.white;
+    }
+}
---
+++ Studio Hoshigaru/Assets/Test Benoit/Skill Tree/Talent Tree/TalentTree.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalentTree : MonoBehaviour
+{
+    private int points = 10;
+
+    [SerializeField]
+    private Talent[] talents;
+
+    [SerializeField]
+    private Talent[] unlockedByDefault;
+
+    [SerializeField]
+    private Text talentPointText;
+
+    public int MyPoint
+    {
+        get
+        {
+            return points;
+        }
+        set 
+        {
+            points = value;
+            UpdateTalentPointText();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTalents();
+    }
+
+    public void TryUseTalent(Talent talent)
+    {
+        if (MyPoint > 0)
+        {
+            MyPoint --;
+        }
+    }
+
+    private void ResetTalents()
+    {
+        foreach (Talent talent in talents)
+        {
+            talent.Lock();
+        }
+
+        foreach (Talent talent in unlockedByDefault)
+        {
+            talent.Unlock();
+        }
+    }
+
+    private void UpdateTalentPointText()
+    {
+        talentPointText.text = points.ToString();
+    }
+}
