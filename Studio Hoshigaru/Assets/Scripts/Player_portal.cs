15046672ceb362f211d60f595e75c7e64df2082d
Aurelia petit ordi <aureila.gomes@epita.fr>
2021-04-28 00:03:16 +0200

    fond fix

--- Studio Hoshigaru/Assets/Scripts/Player_portal.cs
+++ Studio Hoshigaru/Assets/Scripts/Player_portal.cs
@@ -3,25 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class Player_portal : MonoBehaviour
+public class Player_portal : MonoBehaviourPun
 {
     public GameObject Player;
+    public Canvas Vote; 
     
+    public enum Scene {
+        Level1,
+        Level2
+    }
+
+
     private void OnTriggerStay2D(Collider2D collider)
     {
        if (collider.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
         {
-            LoadRandomRoom();   
+            GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+            foreach (var joueur in player)
+            {
+                DontDestroyOnLoad(joueur);
+            }
+
+            SendNotif();
+            //Enlever les //
+            //LoadRandomRoom();
+            
         }
 
     }
 
-    //Tp
+    //Teleportation
     public void LoadRandomRoom()
     {
-        //A faire -> implémenter l'aléatoire
-        PhotonNetwork.LoadLevel(2);
-        Debug.Log("Room Loaded");
+        int nbLvl = Random.Range(0,2); //Je génère une scène aléatoire
+        Scene scene = (Scene)nbLvl; 
+        string lvl = scene.ToString(); //Je prend son string
+        PhotonNetwork.LoadLevel(lvl);
+        Debug.Log("Room Loaded" + lvl );
     }
+
+    public void SendNotif()
+    {
+        base.photonView.RPC("SendMessage", RpcTarget.All, "Hello there");
+    }
+    
+    [PunRPC]
+    void SendMessage(string message)
+    {
+        Vote.gameObject.SetActive(true);
+        Debug.Log("Message envoyé :" + message);
+    }
+
 }
---
+++ Studio Hoshigaru/Assets/Scripts/SpawnRoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnRoom : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] spawnpoint = new GameObject[4];
+
+
+    // Start is called before the first frame update
+    //S'occupe de faire spawn tout le monde à un point différent.
+    void Start()
+    {
+        GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+        int i = 0;
+        foreach (var joueur in player)
+        {
+            GameObject spawn = spawnpoint[i];
+            joueur.transform.position = spawn.transform.position;
+            i++;
+        }
+        
+    }
+
+
+
+}
